032c80f6061fcf83b17df790adff5618974c3c43
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-04-07 01:44:46 +0100

    .

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,29 +1,29 @@
 import { FaBars } from 'react-icons/fa';
 import Logo from './Logo';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { EventType } from '../../models/button';
 
 const NavBar = (): JSX.Element => {
   //* Navigate links
-  const navigate = useNavigate();
-
-  const navigateToHome = (event: EventType) => {
-    event.preventDefault();
-    navigate('/');
-  };
-  const navigateToAbout = (event: EventType) => {
-    event.preventDefault();
-    navigate('/about');
-  };
-  const navigateToInfo = (event: EventType) => {
-    event.preventDefault();
-    navigate('/info');
-  };
-  const navigateToContact = (event: EventType) => {
-    event.preventDefault();
-    navigate('/contact');
-  };
+  const navList = [
+    {
+      name: 'Inicio',
+      path: '/',
+    },
+    {
+      name: 'Sobre',
+      path: '/about',
+    },
+    {
+      name: 'Área de Atuação',
+      path: '/info',
+    },
+    {
+      name: 'Contato',
+      path: '/contact',
+    },
+  ];
 
   //* Change state to control the navbar
   const [nav, setNav] = useState<true | false>(false);
@@ -35,50 +35,43 @@ const NavBar = (): JSX.Element => {
 
   return (
     <nav className="bg-[#F7F7F7] text-[#268FB7]">
-      <div className="hidden sm:flex justify-between items-center text-secondary md:min-h-[40px] font-primary w-full px-14 z-10 ">
+      <div className="hidden sm:flex justify-between items-center md:min-h-[40px] font-primary w-full px-14 z-10 ">
         <div>
           <Logo />
         </div>
-        <ul className="flex px-4 text-base">
-          <li>
-            <button onClick={navigateToHome}>Início</button>
-          </li>
-          <li>
-            <button onClick={navigateToAbout}>Sobre</button>
-          </li>
-          <li>
-            <button onClick={navigateToInfo}>Área de Atuação</button>
-          </li>
-          <li>
-            <button onClick={navigateToContact}>Contato</button>
-          </li>
+        <ul className="flex px-4">
+          {navList.map((item) => (
+            <li className="text-lg">
+              <Link to={item.path} key={item.name}>
+                {item.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
 
       <button className="sm:hidden" onClick={handleNavBar}>
-        <FaBars size={20} className="cursor-pointer mx-0" />
+        <FaBars size={20} className="cursor-pointer mx-0 mt-1" />
       </button>
 
       <div
+        onClick={() => {
+          setNav(!nav);
+        }}
         className={
           nav
-            ? 'sm:hidden overflow-y-hidden ease-in duration-300 left-0 top-0 w-full h-screen opacity-90 px-4 py-7 flex flex-col'
-            : 'absolute top-0 h-screen left-[-100%] ease-in duration-500'
+            ? 'sm:hidden overflow-y-hidden ease-in duration-500 absolute z-50 bg-[#F7F7F7] left-0 top-0 w-full h-screen opacity-95 px-4 py-7 flex flex-col'
+            : 'absolute top-0 h-screen w-full left-[-100%] ease-in duration-500 z-50 bg-[#F7F7F7]'
         }
       >
         <ul className="pt-12 text-center h-full w-full">
-          <li className="py-8">
-            <button onClick={navigateToHome}>Início</button>
-          </li>
-          <li className="py-8">
-            <button onClick={navigateToAbout}>Sobre</button>
-          </li>
-          <li className="py-8">
-            <button onClick={navigateToInfo}>Área de Atuação</button>
-          </li>
-          <li className="py-8">
-            <button onClick={navigateToContact}>Contato</button>
-          </li>
+          {navList.map((item) => (
+            <li className="py-8 text-lg">
+              <Link to={item.path} key={item.name}>
+                {item.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
