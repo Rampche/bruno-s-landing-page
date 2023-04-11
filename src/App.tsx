9a88ab01eb6a5d0255c658f89be4d09f124235ae
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-04-11 13:08:56 +0100

    Intro section started.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Info from './pages/Info';
 
 function App() {
-  return <Outlet />;
+  return (
+    <>
+      <Outlet />
+      <About />
+      <Info />
+      <Contact />
+    </>
+  );
 }
 
 export default App;
