2d196fa12506a8e01dafd74a6bb4b167f5ec0fdb
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-04-26 17:47:01 +0100

    component location changes and other adjustments.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Hero from './home/Hero';
 import Introduction from './home/Introduction';
 import Media from './home/Media';
+import Social from './home/Social';
 
 const Home = (): JSX.Element => {
   return (
     <>
       <Hero />
       <Introduction />
+      <Social />
       <Media />
       {/* //TODO: Números da audiência dele; */}
     </>
--- src/pages/home/Social.tsx
+++ src/pages/home/Social.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 
 const Social = (): JSX.Element => {
-  const followers = [
-    { tikTok: '+150mil' },
-    { instagram: '+350mil' },
-    { views: '1 milhão' },
+  const socialNumbers = [
+    { title: 'Instagram', followers: '+150mil' },
+    { title: 'Tik Tok', followers: '+66mil' },
+    { title: 'Avaliação Google', followers: '5 Estrelas' },
   ];
 
-  return <div></div>;
+  return (
+    <div className="px-6 text-center py-4 font-primary bg-[#f3efec] md:px-14 lg:px-20 leading-loose">
+      <div className="flex-col md:flex-row flex items-center justify-center md:px-14 px-4 font-primary text-[#686868] ">
+        <h2 className="w-1/2 text-3xl font-light">
+          Auxílio diário a milhares de trabalhadores que me seguem e buscam os
+          seus direitos
+        </h2>
+        <div className="w-1/2 p-6">
+          <div className="flex flex-col space-y-4">
+            {socialNumbers.map(({ title, followers }) => (
+              <div
+                key={title}
+                className="flex flex-col justify-center items-center md:flex-row md:space-y-20 text-xl"
+              >
+                <p className="font-bold text-4xl">{followers}</p>
+                <h3 className="font-normal text-[#1a6887]">{title}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Social;
